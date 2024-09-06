bb48fb242dd1c05d7032ecfd92a367442cd25f73
Gavin Cabbage <gavin.cabbage@servicenow.com>
2024-09-06 15:17:01 -0400

    [receiver/awsfirehosereceiver] Add support for CloudWatch logs

---
+++ receiver/awsfirehosereceiver/internal/unmarshaler/cwlog/cwlog.go
@@ -0,0 +1,14 @@
+package cwlog
+
+type cwLog struct {
+	MessageType         string   `json:"messageType"`
+	Owner               string   `json:"owner"`
+	LogGroup            string   `json:"logGroup"`
+	LogStream           string   `json:"logStream"`
+	SubscriptionFilters []string `json:"subscriptionFilters"`
+	LogEvents           []struct {
+		Id        string `json:"id"`
+		Timestamp int64  `json:"timestamp"`
+		Message   string `json:"message"`
+	} `json:"logEvents"`
+}
